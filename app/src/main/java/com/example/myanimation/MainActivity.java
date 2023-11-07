50d8136dd02eb917fea6d15dfa178205bbac4475
a00300334 <a00300334@gmail.com>
2023-11-07 11:08:57 +0000

    anim

---
+++ app/src/main/java/com/example/myanimation/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image);
+    }
+
+    public void scalePressed(View view) {
+        Animation animation = AnimationUtils
+                .loadAnimation(this,R.anim.zoom);
+
+        imageView.startAnimation(animation);
+    }
+}
